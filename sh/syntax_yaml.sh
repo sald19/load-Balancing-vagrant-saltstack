9119fef0a48e63a0ad70c7012c0c40957f348ef0
sald19 <carl.sald19@gmail.com>
2014-01-10 12:18:25 -0600

    agregando seteo de vimrc y corrigiendo syntax la parte de syntax de yaml

--- sh/syntax_yaml.sh
+++ sh/syntax_yaml.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+
+echo "--------------- Instalando vim ----------------"
+apt-get install -y vim
 echo "--------------- Instalando git ----------------"
 apt-get install -y git
 echo "--------------- haciendo clone del repo de syntax  ----------------"
 git clone https://github.com/sald19/salt-vim.git /root/salt-vim.git
-cd salt-vim.git
-echo "--------------- Copiando archivos a .vim de usuario ----------------"
+cd /root/salt-vim.git
+echo "--------------- seteando vimrc  ----------------"
+echo "syntax on" > /root/.vimrc
+echo "--------------- Agregando syntax de YAML a vim ----------------"
 mkdir /root/.vim
-cp -r ftdetect ftplugin syntax  /root/.vim/
+cp -r ftdetect ftplugin syntax /root/.vim/
 rm -rf /root/salt-vim.git/
